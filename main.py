b7d5dd8b38f88c2edd786a841a6178632f7d63fe
Francis Tan <Francis_Tan@discovery.com>
2015-11-03 08:14:39 -0800

    Added block templating system

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, url_for
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def frontpage():
-    return render_template('frontpage.html')
+    return render_template('index.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
