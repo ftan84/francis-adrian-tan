171d486dc06650ead63ce041dbc255675a33c2dd
Francis Tan <Francis_Tan@discovery.com>
2015-11-02 22:19:27 -0800

    Added Bootstrap

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'What up world!'
+def frontpage():
+    return render_template('frontpage.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
